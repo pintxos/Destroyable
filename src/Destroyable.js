c6a1fb022e0d0eb876d5dbc0e34bfbd8afba3d07
Jelle Versele <jelle@versele.be>
2014-08-12 18:25:28 +0200

    Initial commit.

---
+++ src/Destroyable.js
@@ -0,0 +1,52 @@
+/**
+ * Serves as a base layer for all destroyable objects.
+ * Impelements init and destroy methods.
+ */
+
+define([], function () {
+
+	/* Constructor
+	----------------------------------------------- */	
+	var Destroyable = function () {
+		this._isDestroyed = true;
+	};
+	
+
+	/* Methods
+	----------------------------------------------- */	
+
+	/**
+	 * All bootstrap logic should go here
+	 * @return {void}
+	 */
+	Destroyable.prototype.init = function () {
+
+		if(!this.isDestroyed()) {
+			this.destroy();
+		}
+
+		this._isDestroyed = false;
+	};
+
+	/**
+	 * All teardown logic should go here
+	 * @return {void}
+	 */
+	Destroyable.prototype.destroy = function () {
+		this._isDestroyed = true;
+	};
+
+	/**
+	 * Getter for _isDisposed
+	 * @return {Boolean}
+	 */
+	Destroyable.prototype.isDestroyed = function () {
+		return this._isDestroyed;
+	};
+
+
+	/* Export
+	----------------------------------------------- */
+	return Destroyable;
+
+});
