420270d74e378bbaafc7be251256ab499fb38a52
Jelle Versele <jelle@versele.be>
2014-08-17 20:03:38 +0200

    Added some tests.

--- test/index.js
+++ test/index.js
@@ -1,5 +1,30 @@
-describe('test', function () {
-	it('should match', function () {
-		expect(1).toEqual(1);
-	})
+describe('Destroyable', function () {
+
+	var destroyable;
+
+	beforeEach(function () {
+		destroyable = new pintxos.Destroyable();
+	});
+
+	it('isDestroyed() should return true before initialization', function () {
+		expect(destroyable.isDestroyed()).toEqual(true);
+	});
+
+	it('isDestroyed() should return false after initialization', function () {
+		destroyable.init();
+		expect(destroyable.isDestroyed()).toEqual(false);
+	});
+
+	it('isDestroyed() should return true after calling destroy()', function () {
+		destroyable.init();
+		destroyable.destroy();
+		expect(destroyable.isDestroyed()).toEqual(true);
+	});
+
+	it('isDestroyed() should return false after calling init() twice', function () {
+		destroyable.init();
+		destroyable.init();
+		expect(destroyable.isDestroyed()).toEqual(false);
+	});
+	
 });
